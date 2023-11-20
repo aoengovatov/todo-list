3f6119efe0ab83d9dc1dbfe59840e437e35f8837
AlexEngo <a.o.engovatov@yandex.ru>
2023-11-20 11:04:02 +0400

    add loading data

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,13 +1,31 @@
 import styles from './TodoListApp.module.css';
 import { CaseComponent } from './components/CaseComponent/CaseComponent';
+import { useState, useEffect } from 'react';
 
 export const TodoListApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((loadedData) => loadedData.json())
+      .then((loadedTodos) => {
+        setTodos(loadedTodos);
+      })
+      .finally(() => setIsLoading(false))
+  }, []);
   
   return (
     <div className={styles.container}>
       <h1 className={styles.h1}>Список дел</h1>
-      <CaseComponent>Закончить изучение объектов в JS</CaseComponent>
-      <CaseComponent>Сходить в магазин</CaseComponent>
+      {isLoading ? (
+        <div className={styles.loader}></div>
+      ) : (
+        todos.map(({id, title}) => (
+        <CaseComponent key={id}>{title}</CaseComponent>
+      )))}
     </div>
   )
 }
